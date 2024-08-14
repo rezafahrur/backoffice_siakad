e70218015676806ce6132c0a6fa274c36b6a7b91
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2024-08-14 15:59:53 +0700

    feat : add view position crud

---
+++ resources/views/master/position/edit.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.custom')
+
+@section('title', 'Edit User')
+
+@section('content')
+    {{-- start logo and back --}}
+    <nav class="navbar navbar-light">
+        <div class="container d-block">
+            <a href="{{ route('position.index') }}"><i class="bi bi-chevron-left"></i></a>
+            <a class="navbar-brand ms-4" href="{{ route('position.index') }}">
+                <img src="{{ asset('assets/img/logo-kos.svg') }}">
+            </a>
+        </div>
+    </nav>
+    {{-- endt logo and back --}}
+
+    <div class="card-header">
+        <h4 class="card-title">Edit Data</h4>
+    </div>
+    <div class="card-body">
+        <form action="{{ url('master/position/update/'.$position->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <label for="name" class="form-label">Posisi</label>
+                <input type="text" class="form-control" id="posisi" name="posisi"
+                    placeholder="Kode Program Studi" value="{{ $position->posisi }}" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+@endsection
