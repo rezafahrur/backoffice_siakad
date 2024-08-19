d05a4b2c51919155d2f7b4365b701cc2004f30ef
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2024-08-19 10:49:02 +0700

    fix : layout show sdm

--- resources/views/master/hr/show.blade.php
+++ resources/views/master/hr/show.blade.php
@@ -8,7 +8,7 @@
         <div class="container d-block">
             <a href="{{ route('hr.index') }}"><i class="bi bi-chevron-left"></i></a>
             <a class="navbar-brand ms-4" href="">
-                <img style="height: 50px" src="{{ asset('assets/images/logo/logo.png') }}">
+                <img src="{{ asset('assets/img/logo-kos.svg') }}" alt="Logo">
             </a>
         </div>
     </nav>
@@ -21,127 +21,55 @@
             </div>
             <div class="card-body">
                 <div class="row">
-                    {{-- Left Column --}}
-                    <div class="col-md-4">
-                        {{-- kartu pengenal --}}
-                        <div class="card bg-light shadow border-0" style="width: 100%;">
-                            <div class="card-header bg-primary text-white text-center py-3">
-                                <!-- Header content if needed -->
-                            </div>
-                            <div class="card-body text-center">
-                                <p class="card-title mb-0">Politeknik Batu</p>
-                                <img src="{{ asset('storage/' . $hr->photo_profile) }}" alt="Photo Profile" class="rounded-circle mt-3" width="120px">
-                                <h6 class="card-subtitle mt-3">{{ $hr->gelar_depan }} {{ ucwords(strtolower($hr->ktp->nama)) }} {{ $hr->gelar_belakang }}</h6>
-                                <p class="text-muted mb-2">{{ $hr->position->posisi }}</p>
-                            </div>
-                            <div class="card-footer bg-primary text-white text-center py-2">
-                                <small>{{ $hr->ktp->nik }}</small>
-                            </div>
-                        </div>
                     {{-- Photo Profile --}}
                     <div class="col-md-4 d-flex justify-content-center">
-                        <img src="{{ asset('storage/' . $hr->photo_profile) }}" alt="Photo Profile"
-                            class="img-thumbnail mt-2" width="250px" style="height: auto;">
-
+                        <img src="{{ asset('storage/' . $hr->photo_profile) }}" alt="Photo Profile" class="img-thumbnail mt-2" width="250px">
                     </div>
 
-                    {{-- Right Column --}}
+                    {{-- Biodata --}}
                     <div class="col-md-8">
                         <div class="row">
                             {{-- Right Column --}}
-                            <div class="col-md-12">
+                            <div class="col-md-6">
                                 <dl class="row">
-                                    {{-- NIK --}}
-                                    <dt class="col-sm-4"><strong>NIK</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->nik }}</dd>
-
-                                    {{-- nip --}}
-                                    <dt class="col-sm-4"><strong>NIP</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->nip }}</dd>
-
-                                    {{-- Posisi --}}
-                                    <dt class="col-sm-4"><strong>Posisi</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->position->posisi }}</dd>   
+                                    <dt class="col-sm-6"><strong>NIK</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->ktp->nik }}</dd>
+                                    <dt class="col-sm-6"><strong>NIP</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->nip }}</dd>
                                     <dt class="col-sm-6"><strong>Posisi</strong></dt>
                                     <dd class="col-sm-6">{{ $hr->position->posisi }}</dd>
-
-                                    {{-- hp --}}
-                                    <dt class="col-sm-4"><strong>No. HP</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->hrDetail->hp }}</dd>
-
-                                    {{-- Nama Lengkap --}}
-                                    <dt class="col-sm-4"><strong>Nama Lengkap</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->gelar_depan }}  {{ ucwords(strtolower($hr->ktp->nama)) }} {{ $hr->gelar_belakang }} </dd>
+                                    <dt class="col-sm-6"><strong>No. HP</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->hrDetail->hp }}</dd>
                                     <dt class="col-sm-6"><strong>Nama Lengkap</strong></dt>
-                                    <dd class="col-sm-6">{{ $hr->gelar_depan }} {{ ucwords(strtolower($hr->ktp->nama)) }}
-                                        {{ $hr->gelar_belakang }} </dd>
-
-                                    {{-- Alamat Jalan --}}
-                                    <dt class="col-sm-4"><strong>Alamat Jalan</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->alamat_jalan }}</dd>
-
-                                    {{-- alamat_rt --}}
-                                    <dt class="col-sm-4"><strong>RT</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->alamat_rt }}</dd>
-
-                                    {{-- alamat_rw --}}
-                                    <dt class="col-sm-4"><strong>RW</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->alamat_rw }}</dd>
+                                    <dd class="col-sm-6">{{ $hr->gelar_depan }}  {{ ucwords(strtolower($hr->ktp->nama)) }} {{ $hr->gelar_belakang }}</dd>
+                                    <dt class="col-sm-6"><strong>Alamat Jalan</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->ktp->alamat_jalan }}</dd>
+                                    <dt class="col-sm-6"><strong>RT</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->ktp->alamat_rt }}</dd>
                                     <dt class="col-sm-6"><strong>RW</strong></dt>
                                     <dd class="col-sm-6">{{ $hr->ktp->alamat_rw }}</dd>
-
-
-
-
                                 </dl>
                             </div>
-                        </div>
 
-                        <div class="row">
                             {{-- Left Column --}}
-                            <div class="col-md-12">
+                            <div class="col-md-6">
                                 <dl class="row">
-                                    {{-- Provinsi --}}
-                                    <dt class="col-sm-4"><strong>Provinsi</strong></dt>
-                                    <dd class="col-sm-8">{{ $province->name }}</dd>
-                                    
                                     <dt class="col-sm-6"><strong>Provinsi</strong></dt>
                                     <dd class="col-sm-6">{{ $province->name }}</dd>
-
-                                    {{-- Kota/Kabupaten --}}
-                                    <dt class="col-sm-4"><strong>Kota/Kabupaten</strong></dt>
-                                    <dd class="col-sm-8">{{ $city->name }}</dd>
-
-                                    {{-- Kecamatan --}}
-                                    <dt class="col-sm-4"><strong>Kecamatan</strong></dt>
-                                    <dd class="col-sm-8">{{ $district->name }}</dd>
-                                    
+                                    <dt class="col-sm-6"><strong>Kota/Kabupaten</strong></dt>
+                                    <dd class="col-sm-6">{{ $city->name }}</dd>
                                     <dt class="col-sm-6"><strong>Kecamatan</strong></dt>
                                     <dd class="col-sm-6">{{ $district->name }}</dd>
-
-                                    {{-- Tempat Lahir --}}
-                                    <dt class="col-sm-4"><strong>Tempat Lahir</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->lahir_tempat }}</dd>
-
-                                    {{-- Jenis Kelamin --}}
-                                    <dt class="col-sm-4"><strong>Jenis Kelamin</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->jenis_kelamin }}</dd>
-
-                                    {{-- Agama --}}
-                                    <dt class="col-sm-4"><strong>Agama</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->agama }}</dd>
-
-                                    {{-- Golongan Darah --}}
-                                    <dt class="col-sm-4"><strong>Golongan Darah</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->golongan_darah }}</dd>
-
-                                    {{-- Kewarganegaraan --}}
-                                    <dt class="col-sm-4"><strong>Kewarganegaraan</strong></dt>
-                                    <dd class="col-sm-8">{{ $hr->ktp->kewarganegaraan }}</dd>
+                                    <dt class="col-sm-6"><strong>Tempat Lahir</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->ktp->lahir_tempat }}</dd>
+                                    <dt class="col-sm-6"><strong>Jenis Kelamin</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->ktp->jenis_kelamin }}</dd>
+                                    <dt class="col-sm-6"><strong>Agama</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->ktp->agama }}</dd>
+                                    <dt class="col-sm-6"><strong>Golongan Darah</strong></dt>
+                                    <dd class="col-sm-6">{{ $hr->ktp->golongan_darah }}</dd>
                                     <dt class="col-sm-6"><strong>Kewarganegaraan</strong></dt>
                                     <dd class="col-sm-6">{{ $hr->ktp->kewarganegaraan }}</dd>
-
-
                                 </dl>
                             </div>
                         </div>
@@ -149,6 +77,20 @@
                 </div>
             </div>
 
+            {{-- Kartu Pengenal Dosen Politeknik Batu --}}
+            <div class="card-footer text-center">
+                <div class="card bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Politeknik Batu</div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $hr->gelar_depan }} {{ ucwords(strtolower($hr->ktp->nama)) }} {{ $hr->gelar_belakang }}</h5>
+                        <p class="card-text">{{ $hr->position->posisi }}</p>
+                        <img src="{{ asset('storage/' . $hr->photo_profile) }}" alt="Photo Profile" class="img-thumbnail mt-2" width="100px">
+                        <p class="card-text mt-3"><strong>NIP:</strong> {{ $hr->nip }}</p>
+                        <p class="card-text"><strong>NIK:</strong> {{ $hr->ktp->nik }}</p>
+                        <p class="card-text"><strong>No. HP:</strong> {{ $hr->hrDetail->hp }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
