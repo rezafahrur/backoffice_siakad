476162ea81f17d1c9c1ff04745ea9d752b4981e0
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2024-08-22 15:52:50 +0700

    feat : add models berita and kategori berita

---
+++ app/Models/KategoriBerita.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriBerita extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_kategori_berita';
+
+    protected $fillable = [
+        'kategori_berita',
+    ];
+
+    public function berita()
+    {
+        return $this->hasMany(Berita::class, 'kategori_berita_id', 'id');
+    }
+
+    
+}
