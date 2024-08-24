d51b1db584aea8298a8e4be1fdaa114e26f654e5
Reza Fahrur Rasyid <32022957+rezafahrur@users.noreply.github.com>
2024-08-25 04:40:04 +0700

    fix login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hr;
 use App\Models\HrDetail;
 use App\Models\ShortenerURL;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
@@ -63,11 +66,59 @@ class LoginController extends Controller
 
     function prosesLogin(Request $request, $hp, $otp)
     {
-        //proses Login
-        $user = DB::table('t_hr_detail')->where('hp', $hp)->join('m_hr', 't_hr_detail.master_hr_id', '=', 'm_hr.id')->select('t_hr_detail.*', 'm_hr.nama')->first();
+        //get user
+        $user = Hr::join('t_hr_detail', 't_hr_detail.master_hr_id', '=', 'm_hr.id')->join('m_position', 'm_hr.position_id', '=', 'm_position.id')->where('t_hr_detail.hp', $hp)->select('t_hr_detail.*', 'm_hr.nama', 'm_hr.gelar_belakang', 'm_hr.gelar_depan', 'm_position.posisi', 'm_position.id AS position_id')->orderBy('t_hr_detail.created_at', 'desc')->first();
 
-        //return
-        return redirect()->route('/');
+        $hr_detail = HrDetail::where('master_hr_id', $user->master_hr_id)->orderBy('created_at', 'desc')->first();
 
+        if($user->otp == $otp)
+        {
+            //nullkan otp t_hr_detail
+            $hr_detail->otp = null;
+            $hr_detail->save();
+
+            //logout session lama
+            $new_session_id = Session::getId();
+            $last_session = Session::getHandler()->read($user->session_id);
+
+            if ($last_session)
+            {
+                Session::getHandler()->destroy($user->session_id);
+                Auth::guard('hr')->logout();
+            }
+
+            //loginkan
+            $hr_detail->session_id = $new_session_id;
+            $hr_detail->save();
+
+            Session::put('hr_id', $user->master_hr_id);
+            Session::put('nama', explode(" ",$user->nama)[0]);
+            Session::put('nama_lengkap', $user->gelar_depan . ' ' . $user->nama . ' ' . $user->gelar_belakang);
+            Session::put('posisi_id',$user->m_position_id);
+            Session::put('posisi', $user->posisi);
+            Auth::guard('hr')->loginUsingId($user->master_hr_id);
+
+            return redirect()->route('/');
+
+        }
+        else
+        {
+            $hr_detail->otp = null;
+            $hr_detail->session_id = null;
+            $hr_detail->save();
+
+            return redirect()->route('login')->with('error', 'Ada Yang Salah Dalam Link Anda');
+        }
+    }
+
+    function logout()
+    {
+        $user = HrDetail::where('t_hr_detail.master_hr_id', Session::get('hr_id'))->join('m_hr', 't_hr_detail.master_hr_id', '=', 'm_hr.id')->select('t_hr_detail.*', 'm_hr.nama', 'm_hr.gelar_belakang', 'm_hr.gelar_depan', 'm_hr.id AS hr_id')->orderBy('t_hr_detail.created_at', 'desc')->first();
+        Session::getHandler()->destroy($user->session_id);
+        $user->session_id = null;
+        $user->save();
+        Auth::guard('hr')->logout();
+
+        return redirect()->route('login');
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -4,49 +4,47 @@
 
 @push('styles')
 <style>
-    a {
-    text-decoration: none;
+html, body, .container{
+  height: 100%;
 }
-.login-page {
-    width: 100%;
-    height: 100vh;
-    display: inline-block;
-    display: flex;
-    align-items: center;
+
+.login-form {
+  width: 350px;
+  padding: 2rem 1rem 1rem;
 }
-.form-right i {
-    font-size: 100px;
+
+form {
+  padding: 1rem;
 }
 </style>
 @endpush
 
 @section('content')
-<div class="login-page bg-light">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-10 offset-lg-1">
-              <h3 class="mb-3">Login Now</h3>
-                <div class="bg-white shadow rounded">
-                    <div class="row">
-                        <div class="col-md-7 pe-0">
-                            <div class="form-left h-100 py-5 px-5">
-                                <div class="col-12">
-                                    <a href="https://wa.me/6281333662726?text=masuk"  class="btn btn-primary px-4 float-end mt-4">Masuk</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-5 ps-0 d-none d-md-block">
-                            <div class="form-right h-100 bg-primary text-white text-center pt-5">
-                                <i class="bi bi-bootstrap"></i>
-                                <h2 class="fs-1">Welcome Back!!!</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <p class="text-end text-secondary mt-3">Bootstrap 5 Login Page Design</p>
+<div class="container">
+    <div class="wrapper d-flex align-items-center justify-content-center h-100">
+        <div class="card login-form">
+            <div class="card-body">
+                <img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo Politeknik Batu" class="img-fluid">
+                <h5 class="card-title text-center">Back Office</h5>
+                <a href="https://wa.me/6281333662726?text=Masuk" target="_blank" class="btn btn-primary w-100 mt-5">
+                    <i class="bi bi-arrow-return-right"></i> &nbsp;
+                    MASUK
+                </a>
             </div>
         </div>
     </div>
 </div>
-
 @endsection
+
+@push('scripts')
+    <script>
+        @if (session('error'))
+        Swal.fire({
+            title: 'Hmmmm',
+            text: '{{ session('error') }}',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+@endif
+    </script>
+@endpush
