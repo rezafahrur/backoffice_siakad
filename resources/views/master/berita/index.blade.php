38856ce938e3bb869dd64d30b8bb17bd59ee1cb8
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2024-08-25 17:00:58 +0700

    fix : breadcrumb

--- resources/views/master/berita/index.blade.php
+++ resources/views/master/berita/index.blade.php
@@ -16,10 +16,10 @@
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">
-                            <a href="index.html">Dashboard</a>
+                            <a href="index.html">Master</a>
                         </li>
                         <li class="breadcrumb-item active" aria-current="page">
-                            <a href="#">Table</a>
+                            <a href="#">Berita</a>
                         </li>
                     </ol>
                 </nav>
