ac4416c98c7d287f5fcf66463e57b5958afd69cb
YayakFirmansyah <yakizam123@gmail.com>
2024-08-26 14:26:09 +0700

    fix image profile

--- resources/views/layouts/navbar2.blade.php
+++ resources/views/layouts/navbar2.blade.php
@@ -9,7 +9,7 @@
                     class="user-dropdown d-flex align-items-center dropend dropdown-toggle " data-bs-toggle="dropdown"
                     aria-expanded="false">
                     <div class="avatar avatar-md2">
-                        <img src="{{ asset('assets/compiled/jpg/1.jpg') }}" alt="Avatar">
+                        <img src="{{ Session::get('photo_profile') ? asset('storage/' . Session::get('photo_profile')) : asset('storage/2.jpg') }}" alt="Avatar">
                     </div>
                     <div class="text">
                         <h6 class="user-dropdown-name">{{ Session::get('nama') }}</h6>
