b533da4e0c89854c16db566d8227f6ce3f084fd0
satianferdy <satianferdy@gmail.com>
2024-09-18 14:50:49 +0700

    feat: adding kapasitas master kelas

--- resources/views/master/kelas/create.blade.php
+++ resources/views/master/kelas/create.blade.php
@@ -17,26 +17,6 @@
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">Form Create Kelas</h3>
-
-            {{-- Check for validation errors --}}
-            @if ($errors->any())
-                <div class="alert alert-danger alert-dismissible show fade mt-4">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
-
-            {{-- Check for other error messages --}}
-            @if (session('error'))
-                <div class="alert alert-danger alert-dismissible show fade mt-4">
-                    {{ session('error') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
         </div>
         <div class="card-body">
             <form action="{{ route('kelast.store') }}" method="POST" enctype="multipart/form-data">
@@ -167,6 +147,25 @@
                         </div>
                     </div>
                 </div>
+                {{-- kapasitas --}}
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row mb-3">
+                            <label for="kapasitas" class="col-md-3 col-form-label">Kapasitas</label>
+                            <div class="col-md-9">
+                                <input type="text" oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0, 3)"
+                                    name="kapasitas" class="form-control @error('kapasitas') is-invalid @enderror"
+                                    id="kapasitas" value="{{ old('kapasitas') }}">
+                                @error('kapasitas')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <!-- Detail Paket Jadwal -->
                 <div id="kelas-details">
                 </div>
--- resources/views/master/kelas/edit.blade.php
+++ resources/views/master/kelas/edit.blade.php
@@ -17,26 +17,6 @@
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">Form Edit Kelas</h3>
-
-            {{-- Check for validation errors --}}
-            @if ($errors->any())
-                <div class="alert alert-danger alert-dismissible show fade mt-4">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
-
-            {{-- Check for other error messages --}}
-            @if (session('error'))
-                <div class="alert alert-danger alert-dismissible show fade mt-4">
-                    {{ session('error') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            @endif
         </div>
         <div class="card-body">
             <form action="{{ route('kelast.update', $kelas->id) }}" method="POST" enctype="multipart/form-data">
@@ -160,6 +140,24 @@
                                     @endforeach
                                 </select>
                                 @error('kurikulum_id')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- kapasitas --}}
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row mb-3">
+                            <label for="kapasitas" class="col-md-3 col-form-label">Kapasitas</label>
+                            <div class="col-md-9">
+                                <input type="text" oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0, 3)"
+                                    name="kapasitas" class="form-control @error('kapasitas') is-invalid @enderror"
+                                    id="kapasitas" value="{{ old('kapasitas', $kelas->kapasitas) }}">
+                                @error('kapasitas')
                                     <div class="invalid-feedback">
                                         {{ $message }}
                                     </div>
