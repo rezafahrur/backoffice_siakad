9885f5b6785249aa5479dc2a89d422524a56f3c5
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2024-09-20 10:11:34 +0700

    feat : add nilai huruf and bobot

--- resources/views/master/skala-nilai/index.blade.php
+++ resources/views/master/skala-nilai/index.blade.php
@@ -30,6 +30,8 @@
                                     <th>No.</th>
                                     <th>Semester</th>
                                     <th>Program Studi</th>
+                                    <th>Nilai Huruf</th>
+                                    <th>Bobot Maksimum</th>
                                     <th>Tanggal Mulai Efektif</th>
                                     <th>Tanggal Akhir Efektif</th>
                                     <th>Action</th>
@@ -42,6 +44,8 @@
                                         <td>{{ $loop->iteration }}</td>
                                         <td>{{ $skala->semester->nama_semester }}</td>
                                         <td>{{ $skala->programStudi->nama_program_studi }}</td>
+                                        <td>{{ $skala->nilai_huruf }}</td>
+                                        <td>{{ $skala->bobot_maksimum }}</td>
                                         <td>{{ $skala->tgl_mulai_efektif }}</td>
                                         <td>{{ $skala->tgl_akhir_efektif }}</td>
 
