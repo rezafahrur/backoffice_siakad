7f7cad2f36cb621e3119bdbbdb7b1660eb4fe608
Rizaldy Ali Machfuddin <alirizaldy357@gmail.com>
2024-10-15 11:56:23 +0700

    feat: Add nama_singkat and kode_prodi fields to ProgramStudi

--- resources/views/master/prodi/edit.blade.php
+++ resources/views/master/prodi/edit.blade.php
@@ -34,11 +34,34 @@
                 </div>
 
                 <div class="mb-3">
+                    <label for="kode_prodi" class="form-label">Kode Prodi</label>
+                    <input type="text" class="form-control" id="kode_prodi" name="kode_prodi" placeholder="Kode Prodi"
+                        value="{{ old('kode_prodi', $prodi->kode_prodi) }}">
+                    @error('kode_prodi')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
                     <label for="nama_program_studi" class="form-label">Nama Program Studi</label>
                     <input type="text" name="nama_program_studi"
                         class="form-control @error('nama_program_studi') is-invalid @enderror" id="nama_program_studi"
                         value="{{ old('nama_program_studi', $prodi->nama_program_studi) }}">
                     @error('nama_program_studi')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="nama_singkat" class="form-label">Nama Singkat</label>
+                    <input type="text" name="nama_singkat" 
+                        class="form-control @error('nama_singkat') is-invalid @enderror" id="nama_singkat"
+                        placeholder="Nama Singkat" value="{{ old('nama_singkat', $prodi->nama_singkat) }}">
+                    @error('nama_singkat')
                         <div class="invalid-feedback">
                             {{ $message }}
                         </div>
